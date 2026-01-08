919394d77fd6469481a99f495a7bdbcd2c5aecc7
jcheese1 <15570714+jcheese1@users.noreply.github.com>
2026-01-08 22:06:43 +0900

    1024

--- app/components/character-3d.tsx
+++ app/components/character-3d.tsx
@@ -21,14 +21,14 @@ export function Character3D() {
         <mesh rotation={[-Math.PI / 2, 0, 0]}>
           <planeGeometry args={[50, 50]} />
           <MeshReflectorMaterial
-            resolution={256}
+            resolution={1024}
             mixBlur={1}
             mixStrength={80}
             roughness={1}
             depthScale={1.2}
             minDepthThreshold={0.4}
             maxDepthThreshold={1.4}
-            color="#050505"
+            color="#0c0a09"
             metalness={0.5}
           />
         </mesh>
